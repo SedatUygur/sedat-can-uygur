1746486e7d9cadde18787cd61248716ba3920c73
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-22 21:51:39 +0300

    🎉 feat: add navbar and footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className="flex flex-col overflow-x-clip">
+          <Navbar />
+          <main className="flex flex-grow flex-col">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
