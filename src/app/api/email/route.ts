6bf3d175cca1a1cf943cb5766d4f52763f5de6a1
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-23 15:48:26 +0300

    🎉 feat: complete the email route

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -29,4 +29,11 @@ export async function POST(request: NextRequest) {
             }
         });
     });
+
+    try {
+        await sendMailPromise();
+        return NextResponse.json({ message: 'Email sent' });
+    } catch (err) {
+        return NextResponse.json({ error: err }, { status: 500 });
+    }
 }
