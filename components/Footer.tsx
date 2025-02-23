79432ce908d59b227b47fd45352dbf881564ac80
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-23 23:32:14 +0300

    🎉 feat: add icons & refactor footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,60 +1,66 @@
 import React from "react";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faSquareXTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+
+import styles from '../styles/footer.module.css';
 
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white py-8">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row justify-between items-center">
+      <footer className={styles.footerMain}>
+        <div className={styles.container}>
+          <div className={styles.flex}>
             {/* Links Section */}
-            <div className="mb-6 md:mb-0">
-              <h2 className="text-lg font-semibold mb-2">Quick Links</h2>
-              <ul className="space-y-2">
+            <div className={styles.linksSocialMedia}>
+              <h2 className={styles.title2}>Quick Links</h2>
+              <ul className={styles.bottomMenu}>
                 <li>
-                  <Link href="/" className="hover:underline">
+                  <Link href="/" className={styles.bottomLink}>
                     Home
                   </Link>
                 </li>
                 <li>
-                  <Link href="/about" className="hover:underline">
-                    About Us
+                  <Link href="/about" className={styles.bottomLink}>
+                    About
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services" className="hover:underline">
-                    Services
+                  <Link href="/work" className={styles.bottomLink}>
+                    Work
                   </Link>
                 </li>
                 <li>
-                  <Link href="/contact" className="hover:underline">
+                  <Link href="/contact" className={styles.bottomLink}>
                     Contact
                   </Link>
                 </li>
               </ul>
             </div>
-            {/* Social Media Section */}
-            <div className="mb-6 md:mb-0">
-              <h2 className="text-lg font-semibold mb-2">Follow Us</h2>
-              <div className="flex space-x-4">
-                <Link href="#" className="hover:text-gray-400">
-                  Facebook
-                </Link>
-                <Link href="#" className="hover:text-gray-400">
-                  Twitter
-                </Link>
-                <Link href="#" className="hover:text-gray-400">
-                  Instagram
-                </Link>
-                <Link href="#" className="hover:text-gray-400">
-                  LinkedIn
-                </Link>
-              </div>
+            {/* Social Media Section faGithub, faSquareXTwitter, faLinkedin*/}
+            <div className={styles.linksSocialMedia}>
+              <h2 className={styles.title2}>Follow Us</h2>
+              <ul className={styles.bottomMenu}>
+                <li>
+                  <Link href="https://x.com/_SedatUygur" className={styles.bottomLink}>
+                    Twitter <FontAwesomeIcon icon={faSquareXTwitter} size="xl" />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://github.com/SedatUygur" className={styles.bottomLink}>
+                    Github <FontAwesomeIcon icon={faGithub} size="xl" />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://www.linkedin.com/in/sedat-can-uygur/" className={styles.bottomLink}>
+                    LinkedIn <FontAwesomeIcon icon={faLinkedin} size="xl" />
+                  </Link>
+                </li>
+              </ul>
             </div>
             {/* Copyright Section */}
             <div className="text-center md:text-right">
               <p>
-                &copy; {new Date().getFullYear()} Your Company. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} Sedat Can Uygur. All rights reserved.
               </p>
             </div>
           </div>
