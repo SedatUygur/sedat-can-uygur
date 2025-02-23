32c5ec2538870790430fa5b13871b289b7630143
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-23 13:45:53 +0300

    🎉 feat: add contact form to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import styles from "./page.module.css";
+import ContactForm from '@/components/ContactForm';
 
 export default function Home() {
 
   return (
     <div className={styles.page}>
       Home Page
+      <ContactForm />
     </div>
   );
 }
