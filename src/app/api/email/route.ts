eb515d1f62985ddfc12283847422ce6d06460ff4
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-23 15:59:17 +0300

    🎉 feat: send the form data to the email endpoint

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -16,6 +16,7 @@ export async function POST(request: NextRequest) {
     const mailOptions: Mail.Options = {
         from: process.env.MY_EMAIL,
         to: process.env.MY_EMAIL,
+        cc: email,
         subject: `Message from ${name} (${email})`,
         text: message,
     };
