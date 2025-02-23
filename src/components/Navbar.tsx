68f73db446572596f99a8bf6069a218de20aa73f
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-23 12:36:40 +0300

    🎉 feat: refactor navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Icon, Switch, Flex, Spacer } from "@chakra-ui/react"
+import { Icon, Switch } from "@chakra-ui/react"
 import { FaMoon, FaSun } from "react-icons/fa"
+import styles from '../styles/navbar.module.css'
 
 interface NavbarProps {
     toggleTheme: () => void;
@@ -9,36 +10,34 @@ interface NavbarProps {
 
 export default function Navbar({ toggleTheme, currentTheme }: NavbarProps) {
     return (
-      <nav className="navbar" style={{backgroundColor: currentTheme.secondary}}>
-        <Flex>
-            <h2>Sedat Can Uygur</h2>
-            <Spacer />
-            <Flex>
-                <div style={{padding: '0 1rem'}}>
-                    <h2>Home</h2>
+        <div className={styles.navbar} style={{ backgroundColor: currentTheme.secondary, boxShadow: currentTheme.boxShadow }}>
+            <div style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'baseline' }}>
+                <h2 className={styles.logo}>Sedat Can Uygur</h2>
+                <div style={{ display: 'flex' }}>
+                    <div className={styles.navlinks} style={{ padding: '0 1.5rem' }}>
+                        <a>Home</a>
+                    </div>
+                    <div className={styles.navlinks} style={{ padding: '0 1.5rem' }}>
+                        <a>About</a>
+                    </div>
+                    <div className={styles.navlinks} style={{ padding: '0 1.5rem' }}>
+                        <a>Work</a>
+                    </div>
+                    <div className={styles.navlinks} style={{ padding: '0 1.5rem' }}>
+                        <a>Contact</a>
+                    </div>
                 </div>
-                <div style={{padding: '0 1rem'}}>
-                    <h2>About</h2>
-                </div>
-                <div style={{padding: '0 1rem'}}>
-                    <h2>Work</h2>
-                </div>
-                <div style={{padding: '0 1rem'}}>
-                    <h2>Contact</h2>
-                </div>
-            </Flex>
-            <Spacer />
-            <Switch.Root colorPalette="purple" size="lg" onChange={() => toggleTheme()}>
-                <Switch.HiddenInput />
-                <Switch.Control>
-                    <Switch.Thumb />
-                    <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
-                        <Icon as={FaSun} color="yellow.400" />
-                    </Switch.Indicator>
-                </Switch.Control>
-                <Switch.Label></Switch.Label>
-            </Switch.Root>
-        </Flex>
-      </nav>
+                <Switch.Root colorPalette="purple" size="lg" onChange={() => toggleTheme()}>
+                    <Switch.HiddenInput />
+                    <Switch.Control>
+                        <Switch.Thumb />
+                        <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
+                            <Icon as={FaSun} color="yellow.400" />
+                        </Switch.Indicator>
+                    </Switch.Control>
+                    <Switch.Label></Switch.Label>
+                </Switch.Root>
+            </div>
+        </div>
     );
 }
