5f5b4bc8c0334c9b14e349a5c39233dda0b5cc4b
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-23 15:34:03 +0300

    🎉 feat: create a SMTP transport with nodemailer

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,5 +1,12 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json('Hello from API!');
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.MY_EMAIL,
+          pass: process.env.MY_PASSWORD,
+        },
+    });
 }
