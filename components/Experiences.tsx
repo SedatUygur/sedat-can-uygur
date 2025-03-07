45a49815af567355ab2439e52264a1f066ad04a9
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-08 01:42:00 +0300

    🎉 feat : create experience timeline

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -2,50 +2,53 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 
 import { userInfo, headings } from '@/public/js/userInfo';
-import styles from '../styles/about.module.css';
+// import styles from '../styles/about.module.css';
+import styles from '@/src/app/page.module.css';
 
 const Experiences = () => {
   return (
-    <div className={styles.experienceWrapper}>
-      <h1
+    <div className={styles.educationWrapper}>
+      {/* <h1
         className={styles.workHeading}
         data-aos="fade-up"
         data-aos-duration="1000"
         data-aos-delay="100"
       >
         {headings.experience}
-      </h1>
-      <div
-        className={styles.experienceCardWrapper}
-        data-aos="fade-up"
-        data-aos-duration="1000"
-        data-aos-delay="100"
-      >
-        {userInfo.experience.items
-          ? userInfo.experience.items.map((exp, key) => {
-              return (
-                <div className={styles.experienceCard} key={key} data-aos="fade-up">
-                  <Box borderWidth="1px" borderRadius="md" overflow="hidden">
-                    <div className={styles.experienceTitle}></div>
-                    <div className={styles.experienceCardContent}>
-                      <h1>{exp.company}</h1>
-                      {/* <div className={styles.avatar}>
-                                                <Avatar.Root size="xl">
-                                                    <Avatar.Fallback name={exp.company} />
-                                                    <Avatar.Image src={exp.companylogo} />
-                                                </Avatar.Root>
-                                            </div> */}
-                      <div>
-                        <h2>{exp.position}</h2>
-                        <h3>{exp.description}</h3>
-                      </div>
-                      <h4 className={styles.experienceTime}>{exp.time}</h4>
+      </h1> */}
+      <div className={styles.workheading} data-aos="fade-up">
+        {headings.experience}
+      </div>
+      <div className={styles.timeline}>
+        <ul>
+          {userInfo.experience.items
+            ? userInfo.experience.items.map((exp, key) => {
+                return (
+                  <li data-aos="fade-up" key={key}>
+                    <div className={styles.content}>
+                      {/* <Box borderWidth="1px" borderRadius="md" overflow="hidden">
+                        <div className={styles.experienceTitle}></div>
+                        <div className={styles.experienceCardContent}>
+                          <h1>{exp.company}</h1>
+                          <div>
+                            <h2>{exp.position}</h2>
+                            <h3>{exp.description}</h3>
+                          </div>
+                        </div>
+                      </Box> */}
+                      <h3 className={styles.educationDepartment}>{exp.company}</h3>
+                      <h3 className={styles.educationSchool}>{exp.position}</h3>
+                      <p className={styles.educationGPA}>{exp.description}</p>
                     </div>
-                  </Box>
-                </div>
-              );
-            })
-          : null}
+                    <div className={styles.time}>
+                      <h4>{exp.time}</h4>
+                    </div>
+                  </li>
+                );
+              })
+            : null}
+          <div className={styles.clear}></div>
+        </ul>
       </div>
     </div>
   );
